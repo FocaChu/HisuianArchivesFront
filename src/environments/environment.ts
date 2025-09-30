7a51624aefd2bd1c570e1859de32b1be63f8fa7b
FocaChu <joaoguttocordeiro@gmail.com>
2025-09-30 13:24:31 -0300

    chore(deploy): update environment for dev deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://501f70fcc4c0.ngrok-free.app/api',
-  baseUrl: 'https://501f70fcc4c0.ngrok-free.app'
+  apiUrl: 'https://f8cafa943907.ngrok-free.app/api',
+  baseUrl: 'https://f8cafa943907.ngrok-free.app'
 };
