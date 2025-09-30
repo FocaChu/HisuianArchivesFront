e7a247bedce716ef26670ed8ceed73cae41f4b77
FocaChu <joaoguttocordeiro@gmail.com>
2025-09-30 00:29:58 -0300

    fix: login router added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { MainLayoutComponent } from './main/main-layout/main-layout.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    
     {
         path: '',
         component: MainLayoutComponent,
